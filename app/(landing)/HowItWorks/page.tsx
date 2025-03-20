c507c02531abad468d7feddc19c4b233528e138f
@Ndanga <ndangahertier@gmail.com>
2025-03-20 11:55:23 +0200

    Update page.tsx

--- app/(landing)/HowItWorks/page.tsx
+++ app/(landing)/HowItWorks/page.tsx
@@ -289,7 +289,7 @@ const HowItWorks = () => {
                             priority 
                         />
                     </div>
-                    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 z-10 relative">
+                    <div className="max-w-7xl mx-auto px-4 lg:px-8 z-10 relative">
                         <h2 className="text-3xl md:text-4xl font-bold text-white text-center mb-12">
                             How to Request Our Cleaning Services
                         </h2>
